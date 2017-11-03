fa9760531237c33917f6f1185b8f52c5e0765323
ArmsOfSorrow <wave.dev@hotmail.com>
2017-11-04 00:28:39 +0100

    Added edge type

---
+++ src/lumps/edge.rs
@@ -0,0 +1,31 @@
+use std::io::Read;
+use byteorder::{ByteOrder, ReadBytesExt};
+use lumps::LumpData;
+
+#[derive(Debug, PartialEq)]
+#[repr(C)]
+pub struct Edge {
+    pub v: [u16; 2]
+}
+
+impl Edge {
+    pub fn new(v: [u16; 2]) -> Self {
+        Edge { v }
+    }
+}
+
+impl LumpData for Edge {
+    fn load<R: Read, O: ByteOrder>(reader: &mut R) -> ::io::Result<Self> where Self: Sized {
+        let mut buf = [0u16; 2];
+        buf[0] = reader.read_u16::<O>()?;
+        buf[1] = reader.read_u16::<O>()?;
+        
+
+        let edge = Edge::new(buf);
+        Ok(edge)
+    }
+
+    fn get_index() -> usize {
+        12
+    }
+}
--- src/lumps/mod.rs
+++ src/lumps/mod.rs
@@ -4,6 +4,7 @@ use byteorder::ByteOrder;
 
 pub mod vector;
 pub mod plane;
+pub mod edge;
 
 pub trait LumpData {
      fn load<R: Read, O: ByteOrder>(reader: &mut R) -> ::io::Result<Self> where Self: Sized;
